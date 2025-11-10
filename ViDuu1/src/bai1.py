c8784e710cf407f7425f39d652e97741f72a12b0
Phamphuongkhanh <phamphuongkhanh2906@gmail.com>
2025-11-10 22:11:20 +0700

    Add files via upload

---
+++ ViDuu1/src/bai1.py
@@ -0,0 +1,34 @@
+print("1. Hiển thị danh sách nhân viên")
+print("2. Tìm kiếm nhân viên theo mã nhân viên")
+print("3. Tìm kiếm nhân viên theo tên")
+print("4. Tìm kiếm nhân viên theo ngày sinh")
+print("5. Thêm nhân viên mới")
+print("6. Cập nhật thông tin nhân viên")
+print("7. Xoá nhân viên")
+print("8. Lọc nhân viên theo phòng ban")
+print("9. Tính lương trung bình của từng nhân viên")
+print("10. Thống kê số lượng nhân viên theo phòng ban")
+    lua_chon = int(input("Nhập lựa chọn của bạn: "))
+    if lua_chon == 1:
+        print("Thực hiện chức năng: 1. Hiển thị danh sách nhân viên")
+    elif lua_chon == 2:
+        print("Thực hiện chức năng: 2. Tìm kiếm nhân viên theo mã nhân viên")
+    elif lua_chon == 3:
+        print("Thực hiện chức năng: 3. Tìm kiếm nhân viên theo tên")
+    elif lua_chon == 4:
+        print("Thực hiện chức năng: 4. Tìm kiếm nhân viên theo ngày sinh")
+    elif lua_chon == 5:
+        print("Thực hiện chức năng: 5. Thêm nhân viên mới")
+    elif lua_chon == 6:
+        print("Thực hiện chức năng: 6. Cập nhật thông tin nhân viên")
+    elif lua_chon == 7:
+        print("Thực hiện chức năng: 7. Xoá nhân viên")
+    elif lua_chon == 8:
+        print("Thực hiện chức năng: 8. Lọc nhân viên theo phòng ban")
+    elif lua_chon == 9:
+        print("Thực hiện chức năng: 9. Tính lương trung bình của từng nhân viên")
+    elif lua_chon == 10:
+        print("Thực hiện chức năng: 10. Thống kê số lượng nhân viên theo phòng ban")
+    else:
+        print("Lựa chọn sai")
+    print("Vui lòng nhập một số nguyên hợp lệ!")        

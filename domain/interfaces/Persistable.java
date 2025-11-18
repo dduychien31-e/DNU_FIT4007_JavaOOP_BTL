893993dca2b2771b12fb4bdd64529e04d738d35f
Phamphuongkhanh <phamphuongkhanh2906@gmail.com>
2025-11-18 08:50:04 +0700

    Delete interfaces directory

--- domain/interfaces/Persistable.java
+++
@@ -1,8 +0,0 @@
-package com.student.management.interfaces;
-
-import java.io.IOException;
-
-public interface Persistable {
-    void save(String filepath) throws IOException;
-    void load(String filepath) throws IOException;
-}
